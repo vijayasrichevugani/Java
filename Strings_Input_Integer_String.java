7a09d0964424817887db370cc607620450046405
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2022-06-18 15:24:47 -0700

    Update Strings_Input_Integer_String.java

--- Strings_Input_Integer_String.java
+++ Strings_Input_Integer_String.java
@@ -11,6 +11,7 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
+        System.out.println("Enter the String");
         String n = sc.next();
         
         if(isNumber(n)){
