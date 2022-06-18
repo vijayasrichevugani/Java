12a5ca8c4bf7b270f5404665110db1e93ca5df19
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2022-06-17 23:18:37 -0700

    Create MajorElement.java

---
+++ MajorElement.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main
+{
+    public static void MajorityElement(int arr[], int n){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int count =0;
+            if(map.containsKey(arr[i])){
+                count = map.get(arr[i]) +1;
+                if (count > arr.length /2){
+                    System.out.println("Majority element is " + arr[i]);
+                }
+                else{
+                    map.put(arr[i], count);
+                }
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        System.out.println("No Majority Element");
+    }
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Length of array");
+	    int n = sc.nextInt();
+	    int[] arr = new int[n];
+	    for(int i=0;i<arr.length;i++){
+	        System.out.println("Enter an array element");
+	        arr[i] = sc.nextInt();
+	    }
+	    MajorityElement(arr,n);
+	}
+}
