88de79aa9eb6e8e9e6f341a15eae6af19c18abb5
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2022-06-17 19:50:36 -0700

    Create FindNonRepeatedElement.java

---
+++ FindNonRepeatedElement.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main
+{
+    public static ArrayList<Integer> FindNonRepeatedElement(int arr[],int n){
+        HashMap<Integer, Integer> m = new HashMap<>();
+        
+        for(int i=0;i<n;i++){
+            if(!m.containsKey(arr[i])){
+                m.put(arr[i],1);
+            }
+            else{
+                int s = m.get(arr[i]);
+                m.put(arr[i],s+1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            if(m.get(arr[i]) == 1){
+                list.add(arr[i]);
+            }
+        }
+        return list;
+        
+    }
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Length of array");
+	    int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		
+		for(int i=0;i<arr.length;i++){
+		    System.out.println("Enter an array element");
+		    arr[i] = sc.nextInt(); 
+		}
+		
+		
+		System.out.println(FindNonRepeatedElement(arr,n));
+	
+	}
+}
